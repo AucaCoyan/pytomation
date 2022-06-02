8ff48cfc89fea72bd61cc11b8bf7aef9f3d8c44d
Auca Maillot <aucacoyan@gmail.com>
2022-06-02 15:30:24 -0300

    started with terminal.py

--- terminal.py
+++ terminal.py
@@ -1,2 +1,15 @@
-def func():
+import datetime
+
+
+def is_working_day(date) -> bool:
+
+    return True
+
+
+def main():
     return "hello"
+
+
+if __name__ == "__main__":
+    now = datetime.date.today()
+    print(now)
---
+++ test_terminal.py
@@ -0,0 +1,5 @@
+import terminal
+
+
+def test_is_working_day():
+    terminal.is_working_day()
